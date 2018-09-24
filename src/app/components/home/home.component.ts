5eb461045146a185f4a0e1981ea4d489779e07b0
Armin Klacar <42172507+arminklacar@users.noreply.github.com>
2018-09-24 15:55:26 +0200

    Added Env Variables

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log(environment.appUrl);
+  }
 
   ngOnInit() {
   }
