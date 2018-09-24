68e37836f6b26ecd48917d6fddc60f41187a6db0
Armin Klacar <42172507+arminklacar@users.noreply.github.com>
2018-09-24 15:39:01 +0200

    Temporary removed webpack

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,6 @@
 // so the typescript compiler doesn't complain about the global config object
-declare var config: any;
+declare var config:
+    {
+        apiUrl: 'http://localhost:8090', // API BASE URL
+        GOOGLE_CLIENT_ID: '' // GOOGLE CLIENT ID
+    }
